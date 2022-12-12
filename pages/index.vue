58c0a72ed4c579ea8e58b1adf579ab0d758d7c4f
TanyaMkrtchyan <tanymkrtchyan@gmail.com>
2022-12-12 16:48:17 +0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Hero />
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import { axios } from 'axios';
+// const axios = require('axios')
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      movies: []
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=7e44f0e9c2f9e13181ca05a830e626fd&language=en-US&page=1'
+      )
+
+      const result = await data
+      console.log('result ', result);
+    }
+  },
+}
+</script>
